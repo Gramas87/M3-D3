c7e31da8a6ab4407f839531945a0d607305aef2e
Giorgio Colliva <95485973+Gramas87@users.noreply.github.com>
2022-01-26 13:36:39 +0100

    Initial commit

---
+++ M3-D3.js
@@ -0,0 +1,10 @@
+    window.onload = () => {
+    fetch('https://api.pexels.com/v1/search?query=tree',
+      {
+        method: 'GET',
+        headers: ('563492ad6f91700001000001e2948236206a4d278ffe5c700fc3bb68'),
+      })
+      
+  }
+
+      
