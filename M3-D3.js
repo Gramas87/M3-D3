ae6e05950c6e0e5ba21448663660a3d9116d59d7
Giorgio Colliva <95485973+Gramas87@users.noreply.github.com>
2022-01-26 17:02:53 +0100

    finished time

--- M3-D3.js
+++ M3-D3.js
@@ -1,5 +1,5 @@
 
-window.onload = () => {
+const load1 = () => {
   fetch('https://api.pexels.com/v1/search?query=tree',
     {
       method: 'GET',
@@ -7,35 +7,59 @@ window.onload = () => {
     })
     .then((response) => response.json())
     .then((respdata) =>{
+      console.log(respdata)
+      const svg = document.querySelectorAll('svg')
+      for (let i = 0; i < svg.length; i++) {
+        
+        svg[i].classList.add('d-none')
+      }
+      const row = document.querySelector("body > main > div > div > div")
       for (let i = 0; i < respdata.photos.length; i++) {
+        
+        
         let newDiv = document.createElement('div')
-        newDiv.innerHTML = ` 
-            <div class="card mb-4 shadow-sm">
-              <img src="" alt="">
-              <div class="card-body">
-                aaaaaaaaaaaaaaaa
-                <div
-                  class="d-flex justify-content-between align-items-center"
-                >
-                  
-                    <button
-                      type="button"
-                      class="btn btn-sm btn-outline-secondary"
-                    >
-                      hide
-                    </button>
-                  </div>
-                  <small class="text-muted">${respdata.photos[i].id}</small>
-                </div>
-              </div>
-            </div>
-             
-            </div>
+        newDiv.classList.add("col-md-4")
+        newDiv.innerHTML = `
+         
+         <div class="card mb-4 shadow-sm">
+         <img src="${respdata.photos[i].src.small}" alt="">
+         
+         <div class="card-body">
+           <p class="card-text">
+             This is a wider card with supporting text below as a natural
+             lead-in to additional content. This content is a little bit
+             longer.
+           </p>
+           <div
+             class="d-flex justify-content-between align-items-center"
+           >
+             <div class="btn-group">
+               <button
+                 type="button"
+                 class="btn btn-sm btn-outline-secondary"
+               >
+                 View
+               </button>
+               <button
+                 type="button"
+                 class="btn btn-sm btn-outline-secondary" onclick="hide()"
+               >
+                hide
+               </button>
+             </div>
+             <small class="text-muted">${respdata.photos[i].id}</small>
+           </div>
+         </div>
+       </div>
+            
          `
-      document.getElementsByClassName("container1").appendChild(newDiv)
+         
+        row.appendChild(newDiv)
+        
       
       
        }
+       
 
     })
     
@@ -47,3 +71,6 @@ window.onload = () => {
     
     
 }
+
+
+const hide = event => event.target.closest('.card').remove()
